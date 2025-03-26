0a49806829e3a0ad01b7161e042190c0deed479d
prashanthn21 <prashanthnallaveni@gmail.com>
2025-03-26 15:45:26 +0530

    removed the testing audio files

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ function App() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <TextToSpeech />
-  </div>
+  <div className="flex items-center justify-center min-h-screen bg-gray-100">
+     <TextToSpeech />
+   </div>
   );
      
 }
